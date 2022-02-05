68534a58c2925807ccc276de8df9e8a3e8f6bf86
asdfghjkln1 <vicente.rivera.r@usach.cl>
2022-02-05 18:55:24 -0300

    fix calculate hypervolume.R

--- dmmoea_calculate_hypervolume.R
+++ dmmoea_calculate_hypervolume.R
@@ -1,3 +1,70 @@
+calculate_single_hv <- function(){
+  library(ggplot2)
+  #args <- commandArgs(trailingOnly = TRUE)
+  path <- "X:/Universidad/dmmoea" #args[1]
+  results.path <- "Tests/runs/BallHall" #args[2]
+  dataset <- "arabidopsis" #args[3]
+  exp <- "1" #args[4]
+  
+  setwd(path)
+  
+  path <- file.path(path, results.path)
+  print("Path in:")
+  print(file.path(path, "dmnsga2", dataset, paste0(exp, ".csv")))
+  pareto.dmnsga <- read.table(file.path(path, "dmnsga2", dataset, exp, paste0(exp, ".csv")), sep=",", header=TRUE, row.names=NULL)
+  pareto.dnsga <- read.table(file.path(path, "dnsga2", dataset, exp, paste0(exp, ".csv")), sep=",", header=TRUE, row.names=NULL)
+  pareto.nsga <- read.table(file.path(path, "nsga2", dataset, exp, paste0(exp, ".csv")), sep=",", header=TRUE, row.names=NULL)
+  #path <- "X:/Universidad/dmmoea/Tests/runs/BallHall"
+  limits <- read.table(file.path(path, "dmnsga2", "arabidopsis", "limits.csv"), sep=",", header=TRUE, row.names=NULL)
+  scaler.f1 <- function(x){ min(1, (x-limits$min.f1)/(limits$max.f1-limits$min.f1)) }
+  scaler.f2 <- function(x){ min(1, (x-limits$min.f2)/(limits$max.f2-limits$min.f2)) }
+  pareto.dmnsga <- data.frame("f1"=unlist(lapply(pareto.dmnsga[, 1], scaler.f1)), "f2"=unlist(lapply(pareto.dmnsga[, 2], scaler.f2)))
+  pareto.dnsga <- data.frame("f1"=unlist(lapply(pareto.dnsga[, 1], scaler.f1)), "f2"=unlist(lapply(pareto.dnsga[, 2], scaler.f2)))
+  pareto.nsga <- data.frame("f1"=unlist(lapply(pareto.nsga[, 1], scaler.f1)), "f2"=unlist(lapply(pareto.nsga[, 2], scaler.f2)))
+  print(pareto.dmnsga)
+  
+  hv.dmnsga <- calculate_hypervolume(pareto.dmnsga, c(1,1), maximise=FALSE) #eaf::hypervolume(data, c(1,1), maximize=FALSE) 
+  hv.dnsga <- calculate_hypervolume(pareto.dnsga, c(1,1), maximise=FALSE) #eaf::hypervolume(data, c(1,1), maximize=FALSE) 
+  hv.nsga <- calculate_hypervolume(pareto.nsga, c(1,1), maximise=FALSE) #eaf::hypervolume(data, c(1,1), maximize=FALSE) 
+  print(paste0("(dmnsga2, ", exp, ") " ,"hv=", round(hv.dmnsga, 4)))
+  print(paste0("(dnsga2, ", exp, ") " ,"hv=", round(hv.dnsga, 4)))
+  print(paste0("(nsga2, ", exp, ") " ,"hv=", round(hv.nsga, 4)))
+  
+  data <- rbind(rbind(pareto.dmnsga, pareto.dnsga), pareto.nsga)
+  data[, "class"] <- c( rep("dmnsga2", nrow(pareto.dmnsga)) , rep("dnsga2", nrow(pareto.dnsga)) , rep("nsga2", nrow(pareto.nsga)))
+  print(data)
+  gg <- ggplot(data, aes(x=f1, y=f2, group=class, color=class)) + 
+    geom_point() +
+    geom_line(aes(group=class))
+  
+  plot(gg)
+  #hv <- data.frame("Hipervolumen"=c(hv.dmnsga, hv.dnsga, hv.nsga), "Algoritmo"=c("dmnsga2", "dnsga2", "nsga2"))
+  #gg <- ggplot(hv, aes(y=Hipervolumen, group=Algoritmo, fill=Algoritmo)) + 
+  #  geom_bar()
+  #
+  #plot(gg)
+}
+
+#calculate_hv()
+
+test_hypervolume_contribution <- function(){
+  args <- commandArgs(trailingOnly = TRUE)
+  library(rstatix)
+  library(ggplot2)
+  library(ggpubr)
+  path <- args[1]
+  results.path <- args[2]
+  dataset <- args[3]
+  
+  setwd(path)
+  
+  path <- file.path(path, results.path)
+  print("Path in:")
+  print(path)
+  contribution <- read.table(file.path(path, "contribution.csv"), sep=",", header=TRUE, row.names=NULL)
+  hypervolume_comparison_tests(contribution, dataset, path)
+}
+
 calculate_hypervolume_manual <- function(){
   args <- commandArgs(trailingOnly = TRUE)
   
@@ -39,8 +106,9 @@ calculate_hypervolume_manual <- function(){
         contrib <- do.call(paste0, data[, 1:2]) %in% do.call(paste0, ideal.dataset[, 1:2])
         contrib <- sum(contrib)
         #print(paste0("Experiment ", experiment, "has contributed ", contrib, " solutions out of ", nrow.pareto))
-        data <- data.frame("f1"=scaler.f1(data[, 1]), "f2"=scaler.f2(data[, 2]))
+        data <- data.frame("f1"=unlist(lapply(data[, 1], scaler.f1)), "f2"=unlist(lapply(data[, 2], scaler.f2)))
         hv <- calculate_hypervolume(data, c(1,1), maximise=FALSE) #eaf::hypervolume(data, c(1,1), maximize=FALSE) 
+        print(paste0("(", experiment, ") abs=", contrib, " total=", nrow.pareto, " hv=", round(hv, 4)))
         contribution <- rbind(contribution, c(experiment, algorithm, dataset, hv, contrib, nrow.pareto, round(contrib/nrow.pareto, 2)))  
       }
       print(paste(algorithm,dataset, "Mean HV=", mean(hv)))
@@ -48,7 +116,65 @@ calculate_hypervolume_manual <- function(){
   }
   colnames(contribution) <- c("id", "Algorithm", "Dataset", "hypervolume", "contributed", "total", "ratio")
   write.table(contribution, file=file.path(path, "contribution.csv"), sep=",", append=FALSE, quote=FALSE, col.names=TRUE, row.names=FALSE)
+  hypervolume_comparison_tests(contribution, dataset, path)
 }
+
+hypervolume_comparison_tests <- function(data, dataset, output.path, metric="contributed", exp.group="Algorithm"){
+  dir.create(output.path, recursive=TRUE, showWarnings = FALSE)
+  #print("Levels before:")
+  #print(levels(data$Algorithm))
+  data$Algorithm <- factor(data$Algorithm, levels=c("nsga2", "dnsga2", "dmnsga2"))
+  #print("Levels:")
+  #print(levels(data$Algorithm))
+  
+  form <- as.formula(call("~", as.symbol(metric), as.symbol(exp.group)))
+  kruskal.res <- kruskal_test(data, formula=form)
+  pwc <- wilcox_test(data, formula=form, p.adjust.method="bonferroni")
+  pwc <- pwc %>% add_xy_position(x = exp.group)
+  w <- 5 + 0.6*(length(unique(data[, exp.group])) - 3)#ifelse(exp.group>3, 6,5)
+  Y <- pwc$y.position
+  gap.data <- max(data[, metric]) - min(data[, metric])
+  gap <- max(Y[2] - Y[1], gap.data*0.07)
+  pwc$y.position <- Y - gap*seq(from=1, to=0, length.out=length(unique(data[, exp.group])))
+  if(dataset == "arabidopsis"){
+    dataset.name = "Arabidopsis"
+  }else if(dataset == "cell_cycle"){
+    dataset.name = "Cell Cycle"
+  }else if(dataset == "serum"){
+    dataset.name = "Serum"
+  }else if(dataset == "sporulation"){
+    dataset.name = "Sporulation"
+  }
+  
+  if(metric == "contributed"){
+    text.title <- paste0("Soluciones contribuidas a frontera pareto")
+  }else if(metric == "ratio"){
+    text.title <- paste0("Porcentaje contribuci\U00F3n a frontera pareto")
+  }
+  
+  ggplot(data[data$Dataset == dataset, ], aes_string(x=exp.group, y=metric)) +
+    geom_boxplot(aes_string(fill=exp.group)) +
+    labs(subtitle = get_test_label(kruskal.res, detailed = FALSE, p.col="p.adj"), 
+         caption = get_pwc_label(pwc),
+         fill="Algoritmo",
+         title=text.title) +
+    theme_pubr() +
+    scale_fill_manual(labels=c("NSGA-II", "DNSGA-II", "DMNSGA-II"),
+                      values=c("#00AFBB", "#E7B800", "#FC4E07")) +
+    theme(strip.text.x = element_blank(), 
+          axis.text.x = element_blank(),#element_text(angle=25),
+          legend.position="bottom", 
+          plot.subtitle=element_text(size=11),
+          #legend.spacing.x = unit(0, 'cm'),
+          axis.title.x=element_blank()) +
+    guides(fill = guide_legend(label.position = "bottom")) +
+    stat_pvalue_manual(pwc, label = "p = {p.adj}", hide.ns = TRUE)
+  
+  ggsave(file.path(output.path, paste0("contribution_", dataset, ".png")), width = w, height = 7) 
+  
+  print(paste(metric, dataset, "... Done."))
+}
+
 
 calculate_hypervolume <- function(pareto, point, maximise=FALSE){
   pareto <- pareto[order(pareto[, 1], decreasing=TRUE), ]
@@ -129,5 +255,6 @@ calculate_hypervolume_manual_2 <- function(){
   print(hv)
 }
 
+#test_hypervolume_contribution()
 
 calculate_hypervolume_manual()
